691b2a6874f7f72bb6fa10765f52b053660161fb
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-11-27 01:04:16 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,5 +1,5 @@
 #include <catch.hpp>
-#include "stack.hpp"
+#include "newstack.hpp"
 
 TEST_CASE("push") 
 {
@@ -9,10 +9,12 @@ TEST_CASE("push")
     REQUIRE(Stack.count() == 2);
 }
 
-TEST_CASE("pop")
+TEST_CASE("pop") 
 {
-	stack<int> Stack;
-	Stack.push(5);
-	Stack.push(7);
-	REQUIRE(Stack.count() == 2);
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.push(7);
+    Stack.push(9);
+    Stack.pop();
+    REQUIRE(Stack.count() == 2)
 }
